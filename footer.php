5b54cc483f5569db0802b348e8351f483c100cb6
Murilo Sardinha <murilosardinha@gmail.com>
2013-08-03 04:01:03 -0300

    Delete footer.php

--- footer.php
+++
@@ -1,7 +0,0 @@
-<footer class="footer">
-     <p>
-        Copyright &#169; 2013 Sistemas Lineares <br> 
-       Desenvolvido por <a href="http://facebook.com/murilo.sardinha" target="_blank"><u>Murilo Sardinha</u></a>
-    </p>
-
-</footer>
